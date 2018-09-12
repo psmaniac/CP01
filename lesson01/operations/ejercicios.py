94ce7fc7b0aa3c0c66ba2aa2f517586d613808f0
psmaniac <42663733+psmaniac@users.noreply.github.com>
2018-09-12 00:24:41 -0400

    Delete ejercicios.py

--- lesson01/operations/ejercicios.py
+++
@@ -1,9 +0,0 @@
-# Exercises of Lesson01 "operations"
-
-# Please list your answers
-
-# 01 --------------------------------
-#    2 + 3 dividido entre 4
-
-# 02 -------------------------------
-#    78 * 2 divido entre 2 + 7
